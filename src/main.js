145c3dda886ee2f3f5f4864e5654b5e65de0106f
liw <395383468@qq.com>
2018-04-12 17:19:31 +0800

    create agricuture

--- src/main.js
+++ src/main.js
@@ -2,14 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router/'
+import store from './store/'
+import './config/rem'
+import FastClick from 'fastclick'
+
+//适用于移动端点击
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function() {
+    FastClick.attach(document.body);
+  }, false);
+}
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
-})
+}).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
+import App from '../App'
 import Vue from 'vue'
 import Router from 'vue-router'
+import {routerMode} from "../config/env";
 import HelloWorld from '@/components/HelloWorld'
 
-Vue.use(Router)
+//参考网址https://blog.csdn.net/qq_27626333/article/details/76228578
+//vue按需加载组件,数组传入依赖的js ,加载的组件名,模块名称
+const home = r => require.ensure([], () => r(require('@/components/home/home.vue')), 'home')
 
+
+
+
+Vue.use(Router)
 export default new Router({
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop;
+      }
+      return { x: 0, y: to.meta.savedPosition || 0 }
+    }
+  },
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: App,
+      children:[
+        //根目录跳向首页
+        {
+          path: '',
+          redirect: '/home'
+        },
+        {
+          path: '/home',
+          component:home
+        }
+      ]
     }
   ]
 })
+
